ded4da27ab7b55ee7a56c7ba1b72d42775a674f4
flipelunico <flipelunico@gmail.com>
2017-05-17 08:38:12 -0400

    First Version

---
+++ app/src/main/java/com/flipsoft/flipelunico/ordenanzassud/OrdListActivity.java
@@ -0,0 +1,13 @@
+package com.flipsoft.flipelunico.ordenanzassud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrdListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ord_list);
+    }
+}
